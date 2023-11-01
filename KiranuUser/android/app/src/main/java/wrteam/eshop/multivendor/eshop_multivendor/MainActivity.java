171aa5fd2eb0586780e4325124bf8cae5bb02e80
Rajput-surendra <surendraalphawizz@gmail.com>
2023-11-01 20:10:27 +0530

    first commit

---
+++ KiranuUser/android/app/src/main/java/wrteam/eshop/multivendor/eshop_multivendor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fastdy.user;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ KiranuUser/lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = 'Burans';
+
+final String packageName = 'com.fastdy.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.fastdy.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.fastdy.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://fastdy.in/app/v1/api/';
+final String imageUrl = 'https://developmentalphawizz.com/burans/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
